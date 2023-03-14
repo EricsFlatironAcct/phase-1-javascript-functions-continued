63b2ab15569ce961ecac10ce2410833508df645b
Eric Krukowski <erickruk@amazon.com>
2023-03-14 00:18:35 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const saturdayFun = function(act = 'roller-skate'){
+    return `This Saturday, I want to ${act}!`;
+};
+const mondayWork = function(act = 'go to the office'){
+    return `This Monday, I will ${act}.`;
+};
+const wrapAdjective = function(init = '*'){
+    return function(adj = 'special'){
+        return `You are ${init}${adj}${init}!`;
+    }
+};
